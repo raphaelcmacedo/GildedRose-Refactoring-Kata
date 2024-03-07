358589631217e8a0a341f1f0d10bb9d1cceea3b4
raphaelcmacedo <raphaelcmacedo@hotmail.com>
2024-03-07 17:17:26 +0100

    Solution

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,14 +1,98 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 class GildedRose {
+
+    private static final String AGED_BRIE =  "Aged Brie";
+    private static final String SULFURAS =  "Sulfuras";
+    private static final String BACKSTAGE_PASSES = "Backstage passes";
+    private static final String CONJURED = "Conjured";
+    private static final int MIN_QUALITY = 0;
+    private static final int MAX_QUALITY = 50;
+
     Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
+    private boolean canBeSold(Item item){
+        boolean legendary = (item.name != null && item.name.contains(SULFURAS));
+
+        return !legendary;//Easy to add more rules in the future if necessary
+    }
+    private void denoteSellIn(Item item){
+        item.sellIn--;
+    }
+
+    private void denoteQuality(Item item){
+        int factor = this.getQualityFactor(item);
+        boolean decreasing = factor < 0;
+
+        item.quality += factor;
+
+        if(decreasing && item.quality < MIN_QUALITY){//The Quality of an item is never negative
+            item.quality = MIN_QUALITY;
+        }
+
+        if(!decreasing && item.quality > MAX_QUALITY){//The Quality of an item is never more than 50
+            item.quality = MAX_QUALITY;
+        }
+
+    }
+
+    private int getQualityFactor(Item item){
+        int factor = -1;
+
+        if(item.name != null && item.name.contains(AGED_BRIE)){//"Aged Brie" actually increases in Quality the older it gets
+            return 1;
+        }
+
+        if(item.name != null && item.name.contains(BACKSTAGE_PASSES)){//"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+            if(item.sellIn < 0){//Quality drops to 0 after the concert
+                return -item.quality;
+            }
+
+            //Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+            if (item.sellIn <= 5) {
+                return 3;
+            }
+            if (item.sellIn <= 10) {
+                return 2;
+            }
+
+            return 1;//Default value for more than 10 days. Not specified clearly in the task (assuming)
+        }
+
+        if(item.name != null && item.name.contains(CONJURED)){//"Conjured" items degrade in Quality twice as fast as normal items
+            return -2;
+        }
+
+        if(item.sellIn <= 0){//Once the sell by date has passed, Quality degrades twice as fast
+            return -2;
+        }
+
+
+        return factor;
+    }
+
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
+
+        Stream<Item> itemStream = Arrays.stream(items);
+        itemStream.forEach(item -> {
+            if(this.canBeSold(item)){
+                this.denoteSellIn(item);
+                this.denoteQuality(item);
+            }
+        });
+
+
+        //Keeping the old cold commented as reference
+        //Normally I would remove it
+        /*for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
@@ -57,6 +141,6 @@ class GildedRose {
                     }
                 }
             }
-        }
+        }*/
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,96 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void normalItemDegrades() {
+        Item[] items = new Item[] { new Item("foo", 2, 1) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        Item item = items[0];
+        assertEquals("foo", item.name);
+        assertEquals(1, item.sellIn);
+        assertEquals(0, item.quality);
+    }
+
+    @Test
+    void passedItemDegradesTwiceAsFast() {
+        Item[] items = new Item[] { new Item("Passed", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = items[0];
+        assertEquals(-1, item.sellIn);
+        assertEquals(8, item.quality);
+    }
+
+    @Test
+    void qualityIsNeverNegative() {
+        Item[] items = new Item[] { new Item("foo", 1, 0), new Item("boo", 1, -1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertEquals(0, items[0].quality);
+        assertEquals(0, items[1].quality);
+    }
+
+    @Test
+    void agedBrieIncreasesQuality() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 2) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = items[0];
+        assertEquals(1, item.sellIn);
+        assertEquals(3, item.quality);
+    }
+
+    @Test
+    void qualityNoMoreThan50() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = items[0];
+        assertEquals(1, item.sellIn);
+        assertEquals(50, item.quality);
+    }
+
+    @Test
+    void sulfurasCannotBeSold() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = items[0];
+        assertEquals(10, item.sellIn);
+        assertEquals(80, item.quality);
+    }
+
+    @Test
+    void backstagePassesIncreasesQuality() {
+        Item[] items = new Item[] {
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 1),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 1)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertEquals(3, items[0].quality);
+        assertEquals(4, items[1].quality);
+
+    }
+
+    @Test
+    //I am not sure if a passed conjured item should decrease 4x as fast
+    //2x as fast because it has passed and 2x because it is conjured
+    //in doubt I didn't implement this rule
+    void conjuredItemDegradesTwiceAsFast() {
+        Item[] items = new Item[] { new Item("Conjured", 10, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = items[0];
+        assertEquals(9, item.sellIn);
+        assertEquals(8, item.quality);
     }
 
 }
